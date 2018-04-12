ec88a069607d6e56b11804a4795695d3dbeec59f
Olivier Brouard <obrouard@gmail.com>
2018-04-11 19:08:18 -0700

    added new files

---
+++ config.py
@@ -0,0 +1,6 @@
+# Twitter API keys
+consumer_key="fmJ0aWwycNC5DLzrxbi5DlItn"
+consumer_secret="Q4yeAzlCpylKv511EY5ulwCrftoNORSyArIVfI6Np0QGuJqXsP"
+access_token = "749723653672022016-ZP8bQOG8V88e2gmNJUYqB8Yc7Vnnx5X"
+access_token_secret = "4fj3pEcAcpmP82nLTu0txXuEj8STBjmhCJ6M6CAik45Tj"
+
