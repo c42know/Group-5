2ea5b93a979c06a7adce40d470fe404f244e5d9a
Jason Dickey <jd.glenn@ymail.com>
2018-04-16 22:03:16 -0700

    delete config file

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API keys
-consumer_key = "GcPRtcmZsYWeaHkLPVuZq3unL"
-consumer_secret = "4RtXE96PQSLk0OR2ion7kckl9DPqlOX5D4BYp86jRxSNHOEFm6"
-access_token = "980129452108627968-KBomgPRVtedqWw3Gvpx33riCSKQ8BqV"
-access_token_secret = "82X8bWk3FDR0VoT6NvzedFysxYIQWskzBSIAAi1zMQ40V"
